3504f92a1c4875ee001220563d073f723e3e21ec
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 16:09:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,38 +4,21 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Ana sayfayı gönder
+// Dosyaları sun
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Mesaj geçmişi hafızası
-let mesajGecmisi = [];
-
+// Socket bağlantısı
 io.on('connection', (socket) => {
-    
-    // Yeni kullanıcı katılınca
-    socket.on('yeni-kullanici', (isim) => {
-        io.emit('mesaj', {kimden: 'Sistem', icerik: isim + ' sohbete katıldı.', zaman: new Date().toLocaleTimeString()});
-    });
-
-    // Geçmiş mesajları yeni gelen kişiye gönder
-    socket.emit('gecmis-mesajlar', mesajGecmisi);
-
-    // Mesaj gelince
-    socket.on('mesaj', (data) => {
-        data.zaman = new Date().toLocaleTimeString();
-        mesajGecmisi.push(data); // Mesajı hafızaya ekle
-        io.emit('mesaj', data);
-    });
-
-    // Yazıyor özelliği
-    socket.on('yaziyor', (isim) => {
-        socket.broadcast.emit('yaziyor', isim);
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log('Sunucu port ' + PORT + ' üzerinde çalışıyor!');
+    console.log('Sunucu çalışıyor...');
 });
