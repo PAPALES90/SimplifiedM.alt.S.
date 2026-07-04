f738af7123bcf4814b3f801f1245cc24d2c89d34
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 17:13:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,27 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
+var history = []; // Mesajları burada saklıyoruz
+
 io.on('connection', function(socket) {
-    socket.on('login', function(name) { socket.username = name; });
+    // Yeni kişi gelince ona eski mesajları gönder
+    socket.emit('init', history);
+
+    socket.on('login', function(name) {
+        socket.username = name;
+    });
+
     socket.on('chat', function(data) {
-        io.emit('chat', data);
+        var msgObj = { 
+            user: socket.username, 
+            msg: data.msg, 
+            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) 
+        };
+        
+        history.push(msgObj);
+        if(history.length > 50) history.shift(); // Son 50 mesajı tut, daha fazlası şişirir
+        
+        io.emit('chat', msgObj);
     });
 });
 
