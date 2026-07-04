d2333c74e111a18dfddc99216a5afaad4d8328b9
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 16:14:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Dosyaları sun
-app.use(express.static(__dirname));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Socket bağlantısı
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('mesaj', (msg) => {
+        io.emit('mesaj', msg);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log('Sunucu çalışıyor...');
+    console.log('Sunucu ayakta!');
 });
