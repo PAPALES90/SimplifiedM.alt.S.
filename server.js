b2f867f14d12c937158e4e2b8ba38de3336f8afe
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 15:53:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,38 +4,27 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Ana sayfayı gönder
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Mesaj geçmişi hafızası
-let mesajGecmisi = [];
-
 io.on('connection', (socket) => {
-    
-    // Yeni kullanıcı katılınca
-    socket.on('yeni-kullanici', (isim) => {
-        io.emit('mesaj', {kimden: 'Sistem', icerik: isim + ' sohbete katıldı.', zaman: new Date().toLocaleTimeString()});
-    });
+    console.log('Bir kullanıcı bağlandı.');
 
-    // Geçmiş mesajları yeni gelen kişiye gönder
-    socket.emit('gecmis-mesajlar', mesajGecmisi);
-
-    // Mesaj gelince
     socket.on('mesaj', (data) => {
-        data.zaman = new Date().toLocaleTimeString();
-        mesajGecmisi.push(data); // Mesajı hafızaya ekle
-        io.emit('mesaj', data);
+        io.emit('mesaj', {
+            isim: data.isim,
+            mesaj: data.mesaj,
+            zaman: new Date().toLocaleTimeString()
+        });
     });
 
-    // Yazıyor özelliği
-    socket.on('yaziyor', (isim) => {
-        socket.broadcast.emit('yaziyor', isim);
+    socket.on('disconnect', () => {
+        console.log('Bir kullanıcı ayrıldı.');
     });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log('Sunucu port ' + PORT + ' üzerinde çalışıyor!');
+    console.log(`Sunucu ${PORT} portunda çalışıyor.`);
 });
