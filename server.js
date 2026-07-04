047e71b13a66e818c1e125c19f753197f425c743
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 16:46:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-io.on('connection', (socket) => {
-    socket.on('login', (name) => socket.username = name);
-    socket.on('chat', (data) => {
-        io.emit('chat', { user: socket.username, msg: data, time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) });
+var history = []; // Mesajları burada tutacağız
+
+io.on('connection', function(socket) {
+    // Yeni kişi gelince eski mesajları gönder
+    socket.emit('init', history);
+
+    socket.on('login', function(name) {
+        socket.username = name;
+    });
+
+    socket.on('chat', function(data) {
+        var msgObj = { 
+            user: socket.username, 
+            msg: data, 
+            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) 
+        };
+        
+        history.push(msgObj);
+        if(history.length > 50) history.shift(); // Hafızayı şişirmemek için son 50 mesajı tut
+        
+        io.emit('chat', msgObj);
     });
 });
 
