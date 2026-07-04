7441b2524ded7521bc08a5b4addf0cc995916e64
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 11:44:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,34 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// Mesajları hafızada tutacağımız liste
+let mesajGecmisi = [];
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('Bir cihaz bağlandı.');
-    
+    // Yeni biri bağlandığında geçmişi ona gönder
+    socket.emit('gecmis-mesajlar', mesajGecmisi);
+
     socket.on('yeni-kullanici', (isim) => {
         socket.username = isim;
-        io.emit('mesaj', {kimden: 'Sistem', icerik: isim + ' sohbete katıldı.'});
+        const girisMesaji = { kimden: 'Sistem', icerik: isim + ' sohbete katıldı.', zaman: new Date().toLocaleTimeString('tr-TR', {hour: '2-digit', minute:'2-digit'}) };
+        mesajGecmisi.push(girisMesaji);
+        io.emit('mesaj', girisMesaji);
     });
 
     socket.on('mesaj', (data) => {
-        io.emit('mesaj', data);
+        const mesajObjesi = { 
+            kimden: data.kimden, 
+            icerik: data.icerik, 
+            zaman: new Date().toLocaleTimeString('tr-TR', {hour: '2-digit', minute:'2-digit'}) 
+        };
+        mesajGecmisi.push(mesajObjesi);
+        io.emit('mesaj', mesajObjesi);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log('Sunucu ayakta!');
-});
+http.listen(PORT, () => console.log('Sunucu tam gaz çalışıyor!'));
